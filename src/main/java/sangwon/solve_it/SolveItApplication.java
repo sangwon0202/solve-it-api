07596cacffa5d7d84ca6a97376bb919d3b616c34
sangwon0202 <146148180+sangwon0202@users.noreply.github.com>
2024-07-16 21:17:38 +0900

    init

---
+++ src/main/java/sangwon/solve_it/SolveItApplication.java
@@ -0,0 +1,13 @@
+package sangwon.solve_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolveItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sangwon/solve_it/SolveItApplicationTests.java
@@ -0,0 +1,13 @@
+package sangwon.solve_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
