f24ba94cad345686e624da98d76da8dcade9c75f
sangwon0202 <146148180+sangwon0202@users.noreply.github.com>
2024-07-22 21:41:23 +0900

    CORS 대응

---
+++ src/main/java/sangwon/solve_it/config/WebConfig.java
@@ -0,0 +1,24 @@
+package sangwon.solve_it.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${front.endpoint}")
+    private String frontEndPoint;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontEndPoint)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
